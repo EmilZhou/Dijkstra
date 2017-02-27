bbc83e7e194a0897a219cb043868f5e80359d2c3
Emil Zhou <emilzhou@thinkgeo.com>
2017-02-27 20:56:44 +0800

    Update sample

---
+++ Dijkstra/Dijkstra/Edge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    public class Edge
+    {
+        private Node sourceNode;
+
+        public Node SourceNode
+        {
+            get { return sourceNode; }
+            set { sourceNode = value; }
+        }
+
+        private Node targetNode;
+
+        public Node TargetNode
+        {
+            get { return targetNode; }
+            set { targetNode = value; }
+        }
+
+        private int length;
+
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        public Edge(Node source, Node target, int length)
+        {
+            this.SourceNode = source;
+            this.TargetNode = target;
+            this.Length = length;
+        }
+    }
+}
---
+++ Dijkstra/Dijkstra/NodeInformation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    public class NodeInformation
+    {
+        private int dist;
+
+        public int Dist
+        {
+            get { return dist; }
+            set { dist = value; }
+        }
+
+        private NodeState staut;
+
+        public NodeState Staut
+        {
+            get { return staut; }
+            set { staut = value; }
+        }
+
+        public NodeInformation()
+        {
+            dist = int.MaxValue;
+            Staut = NodeState.None;
+        }
+
+    }
+}
